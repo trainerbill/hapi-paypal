ae2dc77a8738ff03e1ba50d2e9405ebbe7b4b4c1
Andrew Throener <athroener@mystartupsolutions.com>
2017-08-04 14:45:04 -0500

    Quick Release Patch

--- src/joi/common.ts
+++ src/joi/common.ts
@@ -7,33 +7,33 @@ const abbrs = us.STATES.map((state: any) => {
 
 export const paypalAddressSchema = joi.object().keys({
     city: joi.string().required(),
-    country_code: joi.string().empty("").max(2).default("US"),
-    line1: joi.string().empty("").required(),
-    line2: joi.string().empty("").optional(),
-    phone: joi.string().empty("").optional(),
-    postal_code: joi.string().empty("").required(),
-    state: joi.string().empty("").allow(abbrs).required(),
+    country_code: joi.string().empty("").trim().max(2).default("US"),
+    line1: joi.string().empty("").trim().required(),
+    line2: joi.string().empty("").trim().optional(),
+    phone: joi.string().empty("").trim().optional(),
+    postal_code: joi.string().empty("").trim().required(),
+    state: joi.string().empty("").trim().allow(abbrs).required(),
 });
 
 export const paypalPhoneSchema = joi.object().keys({
-    country_code: joi.string().regex(/^[0-9]{1,3}?$/).default("1"),
-    national_number: joi.string().regex(/^[0-9]{1,14}?$/),
+    country_code: joi.string().regex(/^[0-9]{1,3}?$/).empty("").trim().default("1"),
+    national_number: joi.string().regex(/^[0-9]{1,14}?$/).empty("").trim().required(),
 });
 
 export const paypalCurrencySchema = joi.object().keys({
-    currency: joi.string().max(3).default("USD"),
-    value: joi.string().required(),
+    currency: joi.string().max(3).empty("").trim().default("USD"),
+    value: joi.string().empty("").trim().required(),
 });
 
 export const paypalTaxSchema = joi.object().keys({
     amount: paypalCurrencySchema.optional(),
-    name: joi.string().optional(),
-    percent: joi.number().min(0).max(100),
+    name: joi.string().empty("").trim().optional(),
+    percent: joi.number().min(0).max(100).optional(),
 });
 
 export const paypalCostSchema = joi.object().keys({
     amount: paypalCurrencySchema.optional(),
-    percent: joi.number().min(0).max(100),
+    percent: joi.number().min(0).max(100).optional(),
 });
 
 export const paypalShippingCostSchema = joi.object().keys({
@@ -43,5 +43,5 @@ export const paypalShippingCostSchema = joi.object().keys({
 
 export const paypalCustomAmountSchema = joi.object().keys({
     amount: paypalCurrencySchema.required(),
-    label: joi.string().max(50).required(),
+    label: joi.string().max(50).empty("").trim().required(),
 });
