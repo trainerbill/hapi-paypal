5f5fd72a9980d288b22951b8a58b209afc73125a
Andrew Throener <athroener@mystartupsolutions.com>
2017-09-19 15:34:24 -0500

    Quick Release Patch

--- src/models/mongoose/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as PaypalWebhook } from "./webhook";
-export { default as PaypalPayment } from "./payment";
-export { default as PaypalInvoice } from "./invoice";
-// export let test = "ok";
